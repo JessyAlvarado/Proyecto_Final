bb3d68000c2a7705fcae0e78a56f8033c870d94a
YIRE NICOLE LOPEZ FLORES <yire.lopez@unah.hn>
2021-12-03 22:46:54 -0600

    Yirè Nicole Lòpez

--- Vistas/AgendarCita.Designer.cs
+++ Vistas/AgendarCita.Designer.cs
@@ -61,9 +61,10 @@ namespace Proyecto_Final.Vistas
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(270, 22);
+            this.label1.Location = new System.Drawing.Point(202, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(353, 44);
+            this.label1.Size = new System.Drawing.Size(281, 36);
             this.label1.TabIndex = 1;
             this.label1.Text = "AGENDAR CITA";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -72,9 +73,10 @@ namespace Proyecto_Final.Vistas
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(239, 100);
+            this.label2.Location = new System.Drawing.Point(179, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 31);
+            this.label2.Size = new System.Drawing.Size(47, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "ID:";
             // 
@@ -82,9 +84,10 @@ namespace Proyecto_Final.Vistas
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(158, 160);
+            this.label3.Location = new System.Drawing.Point(118, 130);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(139, 31);
+            this.label3.Size = new System.Drawing.Size(114, 25);
             this.label3.TabIndex = 3;
             this.label3.Text = "Servicios:";
             // 
@@ -92,9 +95,10 @@ namespace Proyecto_Final.Vistas
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(24, 231);
+            this.label4.Location = new System.Drawing.Point(18, 188);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(259, 31);
+            this.label4.Size = new System.Drawing.Size(216, 25);
             this.label4.TabIndex = 4;
             this.label4.Text = "Nombre de Cliente:";
             // 
@@ -102,9 +106,10 @@ namespace Proyecto_Final.Vistas
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(177, 295);
+            this.label5.Location = new System.Drawing.Point(133, 240);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(106, 31);
+            this.label5.Size = new System.Drawing.Size(87, 25);
             this.label5.TabIndex = 5;
             this.label5.Text = "Correo:";
             // 
@@ -112,9 +117,10 @@ namespace Proyecto_Final.Vistas
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(147, 367);
+            this.label6.Location = new System.Drawing.Point(110, 298);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(150, 31);
+            this.label6.Size = new System.Drawing.Size(122, 25);
             this.label6.TabIndex = 6;
             this.label6.Text = "Dirreciòn:";
             // 
@@ -122,55 +128,56 @@ namespace Proyecto_Final.Vistas
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(12, 451);
+            this.label7.Location = new System.Drawing.Point(9, 366);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(285, 31);
+            this.label7.Size = new System.Drawing.Size(238, 25);
             this.label7.TabIndex = 7;
             this.label7.Text = "Descripciòn del caso:";
             // 
             // IDTXT
             // 
             this.IDTXT.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.IDTXT.Location = new System.Drawing.Point(313, 100);
-            this.IDTXT.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.IDTXT.Location = new System.Drawing.Point(235, 81);
+            this.IDTXT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.IDTXT.Name = "IDTXT";
-            this.IDTXT.Size = new System.Drawing.Size(268, 27);
+            this.IDTXT.Size = new System.Drawing.Size(202, 23);
             this.IDTXT.TabIndex = 8;
             // 
             // NombreClienteTextBox
             // 
             this.NombreClienteTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NombreClienteTextBox.Location = new System.Drawing.Point(312, 224);
-            this.NombreClienteTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.NombreClienteTextBox.Location = new System.Drawing.Point(234, 182);
+            this.NombreClienteTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.NombreClienteTextBox.Name = "NombreClienteTextBox";
-            this.NombreClienteTextBox.Size = new System.Drawing.Size(268, 27);
+            this.NombreClienteTextBox.Size = new System.Drawing.Size(202, 23);
             this.NombreClienteTextBox.TabIndex = 10;
             // 
             // EmailTextBox
             // 
             this.EmailTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EmailTextBox.Location = new System.Drawing.Point(312, 295);
-            this.EmailTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EmailTextBox.Location = new System.Drawing.Point(234, 240);
+            this.EmailTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.EmailTextBox.Name = "EmailTextBox";
-            this.EmailTextBox.Size = new System.Drawing.Size(268, 27);
+            this.EmailTextBox.Size = new System.Drawing.Size(202, 23);
             this.EmailTextBox.TabIndex = 11;
             // 
             // DireccionTextBox
             // 
             this.DireccionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DireccionTextBox.Location = new System.Drawing.Point(312, 371);
-            this.DireccionTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DireccionTextBox.Location = new System.Drawing.Point(234, 301);
+            this.DireccionTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DireccionTextBox.Name = "DireccionTextBox";
-            this.DireccionTextBox.Size = new System.Drawing.Size(268, 27);
+            this.DireccionTextBox.Size = new System.Drawing.Size(202, 23);
             this.DireccionTextBox.TabIndex = 12;
             // 
             // DescripcionProblemaTextBox
             // 
             this.DescripcionProblemaTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DescripcionProblemaTextBox.Location = new System.Drawing.Point(312, 457);
-            this.DescripcionProblemaTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DescripcionProblemaTextBox.Location = new System.Drawing.Point(234, 371);
+            this.DescripcionProblemaTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DescripcionProblemaTextBox.Name = "DescripcionProblemaTextBox";
-            this.DescripcionProblemaTextBox.Size = new System.Drawing.Size(369, 27);
+            this.DescripcionProblemaTextBox.Size = new System.Drawing.Size(278, 23);
             this.DescripcionProblemaTextBox.TabIndex = 13;
             // 
             // ServiciosComboBox
@@ -182,18 +189,18 @@ namespace Proyecto_Final.Vistas
             "Familiar",
             "Mercantiles",
             "Laborales"});
-            this.ServiciosComboBox.Location = new System.Drawing.Point(312, 160);
-            this.ServiciosComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.ServiciosComboBox.Location = new System.Drawing.Point(234, 130);
             this.ServiciosComboBox.Name = "ServiciosComboBox";
-            this.ServiciosComboBox.Size = new System.Drawing.Size(268, 24);
+            this.ServiciosComboBox.Size = new System.Drawing.Size(202, 21);
             this.ServiciosComboBox.TabIndex = 14;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(708, 22);
+            this.pictureBox1.Location = new System.Drawing.Point(531, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(183, 141);
+            this.pictureBox1.Size = new System.Drawing.Size(137, 115);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 15;
             this.pictureBox1.TabStop = false;
@@ -201,9 +208,10 @@ namespace Proyecto_Final.Vistas
             // GuardarButton
             // 
             this.GuardarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.GuardarButton.Location = new System.Drawing.Point(708, 273);
+            this.GuardarButton.Location = new System.Drawing.Point(531, 222);
+            this.GuardarButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.GuardarButton.Name = "GuardarButton";
-            this.GuardarButton.Size = new System.Drawing.Size(166, 72);
+            this.GuardarButton.Size = new System.Drawing.Size(124, 58);
             this.GuardarButton.TabIndex = 16;
             this.GuardarButton.Text = "Guardar";
             this.GuardarButton.UseVisualStyleBackColor = true;
@@ -211,9 +219,10 @@ namespace Proyecto_Final.Vistas
             // NuevoButton
             // 
             this.NuevoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NuevoButton.Location = new System.Drawing.Point(708, 191);
+            this.NuevoButton.Location = new System.Drawing.Point(531, 155);
+            this.NuevoButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.NuevoButton.Name = "NuevoButton";
-            this.NuevoButton.Size = new System.Drawing.Size(166, 72);
+            this.NuevoButton.Size = new System.Drawing.Size(124, 58);
             this.NuevoButton.TabIndex = 17;
             this.NuevoButton.Text = "Nuevo";
             this.NuevoButton.UseVisualStyleBackColor = true;
@@ -221,9 +230,10 @@ namespace Proyecto_Final.Vistas
             // EliminarButton
             // 
             this.EliminarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EliminarButton.Location = new System.Drawing.Point(708, 351);
+            this.EliminarButton.Location = new System.Drawing.Point(531, 285);
+            this.EliminarButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.EliminarButton.Name = "EliminarButton";
-            this.EliminarButton.Size = new System.Drawing.Size(166, 72);
+            this.EliminarButton.Size = new System.Drawing.Size(124, 58);
             this.EliminarButton.TabIndex = 18;
             this.EliminarButton.Text = "Eliminar";
             this.EliminarButton.UseVisualStyleBackColor = true;
@@ -231,9 +241,10 @@ namespace Proyecto_Final.Vistas
             // CancelarButton
             // 
             this.CancelarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CancelarButton.Location = new System.Drawing.Point(708, 429);
+            this.CancelarButton.Location = new System.Drawing.Point(531, 349);
+            this.CancelarButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CancelarButton.Name = "CancelarButton";
-            this.CancelarButton.Size = new System.Drawing.Size(166, 72);
+            this.CancelarButton.Size = new System.Drawing.Size(124, 58);
             this.CancelarButton.TabIndex = 19;
             this.CancelarButton.Text = "Cancelar";
             this.CancelarButton.UseVisualStyleBackColor = true;
@@ -242,11 +253,12 @@ namespace Proyecto_Final.Vistas
             // 
             this.AgendardataGridView.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.AgendardataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.AgendardataGridView.Location = new System.Drawing.Point(0, 508);
+            this.AgendardataGridView.Location = new System.Drawing.Point(0, 413);
+            this.AgendardataGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.AgendardataGridView.Name = "AgendardataGridView";
             this.AgendardataGridView.RowHeadersWidth = 51;
             this.AgendardataGridView.RowTemplate.Height = 24;
-            this.AgendardataGridView.Size = new System.Drawing.Size(942, 127);
+            this.AgendardataGridView.Size = new System.Drawing.Size(706, 103);
             this.AgendardataGridView.TabIndex = 20;
             // 
             // errorProvider1
@@ -255,10 +267,10 @@ namespace Proyecto_Final.Vistas
             // 
             // AgendarCita
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(941, 634);
+            this.ClientSize = new System.Drawing.Size(706, 515);
             this.Controls.Add(this.AgendardataGridView);
             this.Controls.Add(this.CancelarButton);
             this.Controls.Add(this.EliminarButton);
@@ -278,9 +290,8 @@ namespace Proyecto_Final.Vistas
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AgendarCita";
-            this.Text = "Consultasvista";
+            this.Text = "AgendarCita";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.AgendardataGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
