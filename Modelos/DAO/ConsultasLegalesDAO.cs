17fc68e46e4757493adb65109441688489261800
YIRE NICOLE LOPEZ FLORES <yire.lopez@unah.hn>
2021-12-04 07:58:04 -0600

    Yire Nicole Lòpez

--- Modelos/DAO/ConsultasLegalesDAO.cs
+++ Modelos/DAO/ConsultasLegalesDAO.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Proyecto_Final.Modelos.DAO
 {
     class ConsultasLegalesDAO
+
     {
     }
 }
--- Modelos/Entidades/ConsultasLegales.cs
+++ Modelos/Entidades/ConsultasLegales.cs
@@ -8,5 +8,11 @@ namespace Proyecto_Final.Modelos.Entidades
 {
     class ConsultasLegales
     {
+        public int Id { get; set; }
+        public string Servicios { get; set; }
+        public string Nombre { get; set; }
+        public decimal NumeroID { get; set; }
+        public string Descripcion { get; set; } 
+
     }
 }
