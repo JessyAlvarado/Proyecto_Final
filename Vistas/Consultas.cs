377c30a8305350d4e41401547b05b0f4ac6052f3
JessyAlvarado <jmalvaradoe@unah.hn>
2021-12-05 15:20:55 -0600

    Jessy Alvarado

--- Vistas/Consultas.cs
+++ Vistas/Consultas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proyecto_Final.Controladores;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,11 +16,10 @@ namespace Proyecto_Final.Vistas
         public Consultas()
         {
             InitializeComponent();
+            ConsultasLegalesController controller = new ConsultasLegalesController(this);
         }
 
-        private void NuevoButton_Click(object sender, EventArgs e)
-        {
-
-        }
+        
+        
     }
 }
