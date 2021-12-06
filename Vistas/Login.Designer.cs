62d3d3f381a50e41f1cf8dbe0d6d318f72117b5c
JessyAlvarado <jmalvaradoe@unah.hn>
2021-12-06 16:16:07 -0600

    Jessy Alvarado

--- Vistas/Login.Designer.cs
+++ Vistas/Login.Designer.cs
@@ -29,6 +29,7 @@ namespace Proyecto_Final.Vistas
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -37,25 +38,27 @@ namespace Proyecto_Final.Vistas
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button2 = new System.Windows.Forms.Button();
             this.AceptarButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(229, 54);
+            this.label1.Location = new System.Drawing.Point(373, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(294, 36);
+            this.label1.Size = new System.Drawing.Size(209, 62);
             this.label1.TabIndex = 0;
-            this.label1.Text = "INICIAR SESION";
+            this.label1.Text = "LOGIN";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(131, 146);
+            this.label2.Location = new System.Drawing.Point(81, 141);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 22);
             this.label2.TabIndex = 1;
@@ -65,7 +68,7 @@ namespace Proyecto_Final.Vistas
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(105, 219);
+            this.label3.Location = new System.Drawing.Point(37, 212);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(115, 22);
             this.label3.TabIndex = 2;
@@ -74,7 +77,7 @@ namespace Proyecto_Final.Vistas
             // EmailTextBox
             // 
             this.EmailTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EmailTextBox.Location = new System.Drawing.Point(256, 146);
+            this.EmailTextBox.Location = new System.Drawing.Point(174, 141);
             this.EmailTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.EmailTextBox.Name = "EmailTextBox";
             this.EmailTextBox.Size = new System.Drawing.Size(268, 27);
@@ -84,7 +87,7 @@ namespace Proyecto_Final.Vistas
             // ClaveTextBox
             // 
             this.ClaveTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ClaveTextBox.Location = new System.Drawing.Point(256, 214);
+            this.ClaveTextBox.Location = new System.Drawing.Point(174, 207);
             this.ClaveTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ClaveTextBox.Name = "ClaveTextBox";
             this.ClaveTextBox.PasswordChar = '*';
@@ -96,7 +99,7 @@ namespace Proyecto_Final.Vistas
             // 
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.AceptarButton);
-            this.groupBox1.Location = new System.Drawing.Point(584, 82);
+            this.groupBox1.Location = new System.Drawing.Point(494, 104);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -126,12 +129,23 @@ namespace Proyecto_Final.Vistas
             this.AceptarButton.Text = "Aceptar";
             this.AceptarButton.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(698, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(274, 216);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(800, 359);
+            this.ClientSize = new System.Drawing.Size(1001, 391);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.ClaveTextBox);
             this.Controls.Add(this.EmailTextBox);
@@ -142,6 +156,7 @@ namespace Proyecto_Final.Vistas
             this.Name = "Login";
             this.Text = "Login";
             this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,5 +172,6 @@ namespace Proyecto_Final.Vistas
         public System.Windows.Forms.TextBox ClaveTextBox;
         public System.Windows.Forms.Button button2;
         public System.Windows.Forms.Button AceptarButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
